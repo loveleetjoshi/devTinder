0705acff9242cdad7ff00b150f8236e36513d9e8
loveleet.joshi <loveleet.joshi@concirrus.com>
2026-09-21 09:53:57 +0530

    added schema validation and data santization

--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,58 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    password: String,
-    age: Number,
-    gender: String
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 4,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        minLength: 4,
+        maxLength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    age: {
+        type: Number,
+        min: 18
+    },
+    gender: {
+        type: String,
+        validate(value) {
+            if (!["male", "female", "others"].includes(value.toLowerCase())) {
+                throw new Error("Gender data is not valid")
+            }
+        },
+    },
+    photoUrl: {
+        type: String,
+        default: 'https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png',
+        trim: true,
+    },
+    about: {
+        type: String,
+        default: "This is a default about of the user",
+        trim: true,
+    },
+    skills: {
+        type: [String]
+    }
+}, {
+    timestamps: true
 })
 
 const User = mongoose.model("User", userSchema);
